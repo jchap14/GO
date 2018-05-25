05e0030ceff639b79afd877ebb257683fba0c3c7
James <jchap14@gmail.com>
2018-05-24 21:25:39 -0700

    update

--- HOMER_GO_analysis_HUMAN.sh
+++ HOMER_GO_analysis_HUMAN.sh
@@ -1,9 +1,18 @@
-#!/bin/bash -l
+#!/bin/bash
+##### Trim FASTQs for quality & adaptors. FASTQC output generated also
 
-## Variables
+##### for x in `/bin/ls *.untrimmed.R1.fq.gz` ; do bash bash HOMER_GO_analysis_HUMAN.sh $x; done
+
+## add modules (in Conda env, so modules not required)
+# module add perl-scg; module add MEME; module add homer; module add weblogo
+
+## define variables
 FILE=$1
 NAME=`basename $1 .genes`
 
+## write a tempscript to be looped over
+cat > $NAME.tempscript.sh << EOF
+#!/bin/bash -l
 #SBATCH --job-name $NAME.GO
 #SBATCH --output=$NAME.GO.out
 #SBATCH --mail-user jchap14@stanford.edu
@@ -16,18 +25,20 @@ NAME=`basename $1 .genes`
 #SBATCH --nodes=1
 #SBATCH --export=ALL
 
-################### HOMER (findGO.pl): Find GO terms from genelist input
+################### HOMER (findGO.pl): Find GO terms from genelist input 
 ### Prefers refseq IDs if possible, but can convert SYMBOLs
 ## http://homer.salk.edu/homer/motif/
 ## This findGO.pl
-
-# module add perl-scg; module add MEME; module add homer; module add weblogo
-# find *.genes | xargs -n1 sbatch -V -cwd -l h_vmem=4G -pe shm 1 ./HOMER_GO_analysis.sh #cluster
-# find *.genes | xargs -n1 bash HOMER_GO_analysis_HUMAN.sh #local, analyze all
 
 ## commands
 cat $FILE | tr -d '"' > $NAME.temp
 findGO.pl $NAME.temp human $NAME.HOMER_GO -cpu 4
 rm $NAME.temp
+##
+EOF
 
+## qsub then remove the tempscript
+sbatch $NAME.tempscript.sh 
+sleep 1
+rm $NAME.tempscript.sh
 
